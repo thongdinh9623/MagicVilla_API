ff653255f3c333ef4fc31259b2d6dce18fd341fb
Đinh Đức Thông <ducthongdinh@yahoo.com>
2023-07-19 16:02:57 +0700

    Add VillaService.

---
+++ MagicVilla_Web/Services/VillaService.cs
@@ -0,0 +1,68 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Services
+{
+    public class VillaService : BaseService, IVillaService
+    {
+        private readonly string _villaUrl;
+
+        public VillaService(
+            IHttpClientFactory httpClient,
+            IConfiguration configuration) : base(httpClient)
+        {
+            _villaUrl
+                = configuration.GetValue<string>
+                    ("ServiceUrls:VillaAPI");
+        }
+
+        public Task<T> CreateAsync<T>(VillaCreateDTO dto)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = dto,
+                Url = _villaUrl + "/api/VillaAPI"
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(int id)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = $"{_villaUrl}/api/VillaAPI/{id}"
+            });
+        }
+
+        public Task<T> GetAllAsync<T>()
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = $"{_villaUrl}/api/VillaAPI"
+            });
+        }
+
+        public Task<T> GetAsync<T>(int id)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = $"{_villaUrl}/api/VillaAPI/{id}"
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(VillaUpdateDTO dto)
+        {
+            return SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.PUT,
+                Data = dto,
+                Url = $"{_villaUrl}/api/VillaAPI/{dto.Id}"
+            });
+        }
+    }
+}
