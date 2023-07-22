f3de882483b9e7de96b5da5aa37ae1bc3a49b4ab
Đinh Đức Thông <ducthongdinh@yahoo.com>
2023-07-22 23:14:10 +0700

    Calling Villa API.

---
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService? _villaService;
+        private readonly IMapper? _mapper;
+
+        public VillaController(
+            IVillaService villaService,
+            IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> list = new();
+            APIResponse response = await _villaService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                string? responseResult = Convert.ToString(response.Result);
+                list = JsonConvert
+                    .DeserializeObject<List<VillaDTO>>
+                        (responseResult);
+            }
+
+            return View(list);
+        }
+    }
+}
--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -1,10 +1,14 @@
 using MagicVilla_Web;
+using MagicVilla_Web.Services;
+using MagicVilla_Web.Services.IServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
+builder.Services.AddHttpClient<IVillaService, VillaService>();
+builder.Services.AddScoped<IVillaService, VillaService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,4 +24,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
